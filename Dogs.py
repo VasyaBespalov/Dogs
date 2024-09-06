4091283a562533013b85cb287f8fdf5a1330fe0c
VasyaBespalov <89515356483@mail.ru>
2024-09-06 12:12:28 +0300

    Создана функция загрузки ссылки на изображения

--- Dogs.py
+++ Dogs.py
@@ -5,6 +5,17 @@ from PIL import Image, ImageTk
 from io import BytesIO
 
 
+def get_dog_image():
+    try:
+        response = requests.get("https://dog.ceo/api/breeds/image/random")  # в response получаем json
+        response.raise_for_status()  # Если все хорошо, то статус 200. Если ошибка showerror покажет
+        data = response.json()  # В data лежит ответ в формате json
+        return data('message')  # Функция get_dog_image вернет информ по ключу message, адрес картинки
+    except Exception as e:
+        mb.showerror("Ошибка", f"Возникла ошибка при запросе к API {e}")
+        return None  # Обязательно возвращаем ответ. Если возвращать нечего, то None
+
+
 def show_image():
     image_url = get_dog_image()
     if image_url:
@@ -12,18 +23,13 @@ def show_image():
             response = requests.get(image_url, stream=True)  # Ответ будет равен запросу. В ответ получим что-то загруженное по этой ссылке
             response.raise_for_status()  # Получаем статус ответа, пригодится для обработки исключений
             img_data = BytesIO(response.content)  # Теперь по этой ссылке с помощью BytesIO загрузили изображение в двоичном коде
-            img = Image.open(img_data)  # С помощью PIL обрабатываем. Теперь здесь ккртинка
-            img.thumbnail((300,300))  # Загруженная картинка будет подогнана под размер 300x300
-            label.config(Image=img)
+            img = Image.open(img_data)  # С помощью PIL обрабатываем. Теперь здесь картинка
+            img.thumbnail((300, 300))  # Загруженная картинка будет подогнана под размер 300x300
+            img = ImageTk.PhotoImage(img)  #
+            label.config(image=img)
             label.image = img  # Чтобы картинку сборщик мусора не собрал
         except Exception as e:
-            mb.showerror("Ошибка", f"Возникла ошибка {e}")
-
-
-
-
-
-
+            mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения {e}")
 
 
 window = Tk()
